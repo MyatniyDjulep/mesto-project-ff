117ec32d48de1a4a98c259b28d7c6d53526551f2
Alexandr <tulchan@yandex.ru>
2024-09-08 23:51:16 +0300

    review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const templateCard = document.querySelector('#card-template').content;
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(linkCard, cardTitle, deleteCardFunction) {
+  const cardElement = templateCard.querySelector('.card').cloneNode(true);
+  const cardElementLink = cardElement.querySelector('.card__image');
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
+  cardElementLink.src = linkCard;
+  cardElementLink.alt = 'photo';
+
+  deleteButton.addEventListener('click', () => deleteCardFunction(cardElement));
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function removeCard(card) {
+  card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  const card = createCard(item.link, item.name, removeCard);
+  cardList.append(card);
+});
